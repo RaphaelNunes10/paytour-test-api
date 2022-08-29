461d991bc1eca4acfde36d2a64939ccf62bbb030
RaphaelNunes10 <raphaelnunes10@outlook.com>
2022-08-28 17:18:54 -0700

    Remove CORS middleware

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['cors'])->group(function () {
-    // Curricula
+// Curricula
 
-    Route::get('curricula', "App\Http\Controllers\curriculaController@index");
-    Route::post('curriculum/register', "App\Http\Controllers\curriculaController@store");
-    Route::patch('curriculum/{curriculum}', "App\Http\Controllers\curriculaController@update");
-    Route::delete('curriculum/{curriculum}', "App\Http\Controllers\curriculaController@remove");
+Route::get('curricula', "App\Http\Controllers\curriculaController@index");
+Route::post('curriculum/register', "App\Http\Controllers\curriculaController@store");
+Route::patch('curriculum/{curriculum}', "App\Http\Controllers\curriculaController@update");
+Route::delete('curriculum/{curriculum}', "App\Http\Controllers\curriculaController@remove");
 
-    // Mails
+// Mails
 
-    Route::get('/send/email', 'MailController@mail');
-});
+Route::get('/send/email', 'MailController@mail');
